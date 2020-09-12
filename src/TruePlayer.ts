251aba4f1198571766a55a6037942db79c206d29
Moein Alizadeh <truemoein@gmail.com>
2020-09-12 21:21:55 +0430

    Add Website.

--- src/TruePlayer.ts
+++ src/TruePlayer.ts
@@ -1,6 +1,18 @@
 class TruePlayer {
-  constructor() {
-    console.log('test');
+  private video: HTMLVideoElement;
+
+  constructor(id: string) {
+    this.video = document.getElementById(id) as HTMLVideoElement;
+  }
+
+  public play(): Promise<unknown> {
+    return this.video.play();
+  }
+
+  public seek(time: number, andPlay = false): Promise<unknown> | void {
+    this.video.currentTime = time;
+    if (andPlay) return this.play();
+    return undefined;
   }
 }
 
